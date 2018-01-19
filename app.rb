9bdd4d637b29c2aff835bbec91640558d1fd631a
ronald vilorio <soccer23415@gmail.com>
2018-01-19 19:25:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/display_puppy' do
     erb :display_puppy
   end
-  get '/create_puppy' do
+  get '/new' do
     erb :create_puppy
   end
   post '/display_puppy' do
